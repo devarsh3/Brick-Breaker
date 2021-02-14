7d47fc074a450a6a19aeebdb8d45431f30359362
devarsh3 <63943441+devarsh3@users.noreply.github.com>
2021-02-14 05:20:15 -0500

    Add files via upload

---
+++ src/brickBreaker/Main.java
@@ -0,0 +1,25 @@
+package brickBreaker;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+
+public class Main{
+
+	public static void main(String[] args) {
+		JFrame obj = new JFrame();
+		Gameplay gamePlay = new Gameplay();
+		obj.setBounds(10,10,700,600);
+		obj.setTitle("Breakout Ball");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		obj.setLocation(dim.width/2-obj.getSize().width/2, dim.height/2-obj.getSize().height/2);
+		obj.add(gamePlay);
+
+	}
+
+}
